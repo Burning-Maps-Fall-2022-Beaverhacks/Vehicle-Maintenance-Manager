3e6f4b6b07a3dd429ccef1c7af512be747783384
Alex Hollinghead <alexhollinghead@pm.me>
2022-10-07 16:31:32 -0400

    Created json object to put to db (API test)

--- maintenance_app/views.py
+++ maintenance_app/views.py
@@ -2,8 +2,10 @@
 A view function is the code you write to respond to requests.
 Flask uses patterns to match the incoming request URL to the view that should handle it.
 """
+
 from flask import current_app as app
 import requests
+import json
 
 
 # A simple page that says hello
@@ -24,6 +26,35 @@ header = {
 
 @app.route('/api-test')
 def api_test():
-    vin = requests.get(
-        'http://api.carmd.com/v3.0/decode?vin=1GNALDEK9FZ108495', headers=header)
-    return f'{vin.json()}'
+    # vin_request = requests.get(
+    #     'http://api.carmd.com/v3.0/decode?vin=1GNALDEK9FZ108495', headers=header)
+    # request_json = vin_request.json()
+    request_json = {
+        "message": {
+            "code": 0,
+            "message": "ok",
+            "credentials": "valid",
+            "version": "v3.0.0",
+            "endpoint": "decode",
+            "counter": 5
+        },
+        "data": {
+            "year": 2015,
+            "make": "CHEVROLET",
+            "model": "EQUINOX",
+            "manufacturer": "GENERAL MOTORS",
+            "engine": "L4, 2.4L; DOHC; 16V; DI; FFV",
+            "trim": "LTZ",
+            "transmission": "AUTOMATIC"
+        }
+    }
+    vin = '1GNALDEK9FZ108495'
+    make = request_json['data']['make']
+    model = request_json['data']['model']
+    manufacturer = request_json['data']['manufacturer']
+    transmission = request_json['data']['transmission']
+    year = request_json['data']['year']
+    engine = request_json['data']['engine']
+    trim = request_json['data']['trim']
+    return f'VIN: {vin}, Make: {make}, Model: {model}, Manufacturer: {manufacturer},\
+        Transmission: {transmission}, Year: {year}, Engine: {engine}, Trim: {trim}'
