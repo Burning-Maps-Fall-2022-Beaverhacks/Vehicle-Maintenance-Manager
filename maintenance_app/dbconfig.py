8b009d44ef6591e63180e817139bebeaf73c069c
Eric Kelly <ekellyhawks@gmail.com>
2022-10-08 16:25:00 -0500

    added vehicle color field

--- maintenance_app/dbconfig.py
+++ maintenance_app/dbconfig.py
@@ -25,7 +25,7 @@ def create_tables(connection_obj):
                         windshield_wiper_rear_size TEXT, 
                         engine_oil_viscosity TEXT, 
                         engine_oil_capacity REAL, 
-                        tire_pressure INT); """
+                        vehicle_color TEXT); """
 
     create_owner_table = """CREATE TABLE owner  ( 
                         owner_id INTEGER PRIMARY KEY AUTOINCREMENT,
