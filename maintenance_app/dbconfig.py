ff8ded9841196ae5fbd0a791c8999b1d612a7b5c
Alex Hollinghead <alexhollinghead@pm.me>
2022-10-09 18:26:25 -0400

    Added redirect for add vehicle

--- maintenance_app/dbconfig.py
+++ maintenance_app/dbconfig.py
@@ -83,19 +83,17 @@ def create_tables(connection_obj):
                     maintenance_date datetime, 
                     status TEXT); """
 
-
-
     create_table_queries = [create_vehicle_table, create_owner_table,
-                            create_owned_vehicle_table, create_recall_table, 
+                            create_owned_vehicle_table, create_recall_table,
                             create_maintenance_table]
 
     for create_statement in create_table_queries:
         cursor_obj.execute(create_statement)
 
-
-    # insert user 
-    owner = (None, "Eric", "Kelly", "Eric Kelly", "kelleric@oregonstate.edu", "unencryptedpassword")
-    cursor_obj.executemany('INSERT INTO owner values (?,?,?,?,?,?)', owner)
+    # insert user
+    owner = (None, "Eric", "Kelly", "Eric Kelly",
+             "kelleric@oregonstate.edu", "unencryptedpassword")
+    cursor_obj.execute('INSERT INTO owner values (?,?,?,?,?,?)', owner)
     connection_obj.commit()
     print("Vehicle, owner, owned_vehicle, recall, and maintenance tables are ready")
 
